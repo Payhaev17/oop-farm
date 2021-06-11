0e13215bc0b5431e91585d04ec6cdd4ecd9a7289
Mota <75909585+Payhaev17@users.noreply.github.com>
2021-06-11 22:48:33 +0300

    Update main.php

--- main.php
+++ main.php
@@ -7,7 +7,7 @@ require "./classes/Chicken.php";
 require "./classes/InfoPrinter.php";
 require "./classes/Farm.php";
 
-// Если требуется новое животное, то добавить в массив новый элемент с названием класса и количеством животного
+// Если требуется новое животное, то нужно добавить в массив новый элемент с названием класса и количеством животного
 $Farm = new Farm([
   ["animalType" => "Cow", "quantity" => 10],
   ["animalType" => "Chicken", "quantity" => 20]
