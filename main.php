38278271ea782c229ed5a72558da1c790e4f1dd2
Payhaev17 <ran777dom@gmail.com>
2021-06-12 14:12:13 +0300

    changes

--- main.php
+++ main.php
@@ -7,7 +7,7 @@ require_once __DIR__."/vendor/autoload.php";
 // Если требуется новое животное, то нужно добавить в массив новый элемент с названием класса и количеством животного
 $Farm = new Farm();
 
-// Можно и так (Название класса, количество)
+// (Название класса, количество)
 $Farm->addAnimal(new Animals\Cow(), 20);
 $Farm->addAnimal(new Animals\Chicken(), 10);
 $Farm->addAnimal(new Animals\Sheep(), 15);
